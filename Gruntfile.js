8617505e3aad4964c336535b9df725e42a9f925d
Alexander Leanovich <a.leanovich@yandex.ru>
2016-11-11 14:32:31 +0300

    project structure create

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+var config = require('./config');
+
+module.exports = function(grunt) {
+
+	grunt.loadNpmTasks('grunt-screeps');
+
+    grunt.initConfig({
+        screeps: {
+            options: {
+                email: config.email,
+                password: config.password,
+                branch: config.branch,
+                ptr: false
+            },
+            dist: {
+                src: ['src/*.js']
+            }
+        }
+    });
+
+
+    grunt.registerTask('default', ['screeps']);
+}

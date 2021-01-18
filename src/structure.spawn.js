a172fc34804b4a84d06a202cda86882dee99d5fc
atlantech <a.leanovich@yandex.ru>
2021-01-18 15:16:26 +0300

    fix creeps access

--- src/structure.spawn.js
+++ src/structure.spawn.js
@@ -2,7 +2,7 @@ var _ = require('lodash');
 
 module.exports = {
     run: function() {
-        if (Game.creeps.keys.length < 4) {
+        if (Object.keys(Game.creeps).length < 4) {
             _.each(Game.spawns, function (spawn) {
                 const result = spawn.spawnCreep([MOVE, WORK, CARRY, ATTACK]);
 
